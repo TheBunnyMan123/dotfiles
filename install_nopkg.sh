8562d89f8e85882652b016a15caa297a6a551fbc
TheBunnyMan123 <teguemcnair@gmail.com>
2024-02-08 10:09:55 -0600

    Create install_nopkg.sh

---
+++ install_nopkg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+git clone https://github.com/TheBunnyMan123/dotfiles.git $TMPDIR
+ls $TMPDIR
+cp -r -v $TMPDIR/ $HOME/
