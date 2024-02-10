1734fb27ee0a9c07d1f5ca45026b1f1cc5af851c
TheBunnyMan123 <teguemcnair@gmail.com>
2024-02-10 01:12:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,3 +35,5 @@ yes | sudo pacman -R gnome-console
 sudo mkdir -p /usr/local/share/gnome-terminal-symlink/
 sudo curl https://gist.githubusercontent.com/TheBunnyMan123/08d10eeea24a00fec46c926aaa484482/raw/gterm-alacritty.sh -o /usr/local/share/gnome-terminal-symlink/gterm-alacritty.sh
 sudo ln /usr/local/share/gnome-terminal-symlink/gterm-alacritty.sh /usr/local/bin/gnome-terminal
+sudo chmod 755 /usr/local/share/gnome-terminal-symlink/gterm-alacritty.sh
+sudo chmod 755 /usr/local/bin/gnome-terminal
